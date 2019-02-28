194fcc61e48a3e9910589196ac659b4e7ee73e7f
loraineamaral <loraineeamaral@gmail.com>
2019-02-28 19:45:22 -0300

    sort function

--- src/main.js
+++ src/main.js
@@ -26,30 +26,30 @@ function showPokemons() {
 
 let btnType = document.getElementsByClassName("btn-types")
 
-for (button of btnType){
+for (button of btnType) {
   let btnId = button.id;
-  console.log("BOTAO AIDI", btnId)
- 
-  button.addEventListener('click', ()=>{
-    document.getElementById("list-poke").innerHTML="";  
+  // console.log("BOTAO AIDI", btnId)
+
+  button.addEventListener('click', () => {
+    document.getElementById("list-poke").innerHTML = "";
     filterPoke(btnId);
-  } );
+  });
 }
 
-function filterPoke(btnId){
-  
-  for (let i in POKEMON["pokemon"]){
+function filterPoke(btnId) {
+
+  for (let i in POKEMON["pokemon"]) {
     let pokeObj = POKEMON["pokemon"][i];
     let types = POKEMON["pokemon"][i]["type"];
 
-    for(j of types){
-      if (btnId === j){
-        console.log("JOTA", j)
-        console.log("botaoIDDD", btnId)
-       
-      
-let pokemonDivFil = document.getElementById("list-poke");
-pokemonDivFil.innerHTML +=  `
+    for (j of types) {
+      if (btnId === j) {
+        // console.log("JOTA", j)
+        // console.log("botaoIDDD", btnId)
+
+
+        let pokemonDivFil = document.getElementById("list-poke");
+        pokemonDivFil.innerHTML += `
     <div class="pokemon-unit">
       <img src="${pokeObj["img"]}" class="poke-img"/>
       <div class= "poke-namenum">
@@ -61,5 +61,42 @@ pokemonDivFil.innerHTML +=  `
       }
     }
   }
-}  
+}
 
+let selectOpt = document.getElementsByTagName("select").selectedIndex;
+console.log(selectOpt);
+selectOpt.addEventListener("change", sortPoke);
+
+function sortPoke() {
+  let namePoke = [];
+
+  for (i in POKEMON["pokemon"]) {
+    namePoke.push(POKEMON["pokemon"][i]["name"]);
+    if (selectOpt === 1) {
+      console.log("passou")
+      console.log("namePoke")
+      console.log(selectOpt)
+      namePoke.sort();
+    }
+    else if (selectOpt === 2) {
+      console.log("passounoelseif")
+      namePoke.sort();
+      namePoke.reverse();
+    }
+    // else{
+    //   showPokemons();
+    // }
+
+//     let pokemonDivFil = document.getElementById("list-poke");
+//     pokemonDivFil.innerHTML += `
+// <div class="pokemon-unit">
+//   <img src="${namePoke["img"]}" class="poke-img"/>
+//   <div class= "poke-namenum">
+//     <p class="poke-num"> Nº ${namePoke["num"]}</p>
+//     <h3 class="poke-name">${namePoke["name"]}</h3>
+//   </div> 
+// </div>
+// `
+  }
+
+}
