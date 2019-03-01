363b03cfec94c94dd675b51b38abb0cbf8f5c0b0
loraineamaral <loraineeamaral@gmail.com>
2019-03-01 19:23:34 -0300

    funcao sort funciona

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 window.onload = function () {
-  showPokemons();
+  showPokemons(getPokemons());
 
 };
+
+function clearScreen(){
+document.getElementById("list-poke").innerHTML = ""
+}
 
 function getPokemons() {
   return POKEMON["pokemon"];
@@ -9,13 +13,14 @@ function getPokemons() {
 
 // console.log("Lista de Pokemons: ", getPokemons())
 
-function showPokemons() {
+function showPokemons(getPoke) {
   let pokemonDiv = document.getElementById("list-poke");
   pokemonDiv.innerHTML = `
-    ${getPokemons().map((pokelist) => `
+    ${getPoke.map((pokelist) => `
       <div class="pokemon-unit">
         <img src="${pokelist["img"]}" class="poke-img"/>
         <div class= "poke-namenum">
+        <div class="poke-shadow"></div>
           <p class="poke-num"> Nº ${pokelist["num"]}</p>
           <h3 class="poke-name">${pokelist["name"]}</h3>
         </div> 
@@ -63,40 +68,46 @@ function filterPoke(btnId) {
   }
 }
 
-let selectOpt = document.getElementsByTagName("select").selectedIndex;
-console.log(selectOpt);
-selectOpt.addEventListener("change", sortPoke);
+let selectOpt = document.querySelector("select");
+selectOpt.addEventListener("change", () => 
+sortPoke(selectOpt.selectedIndex)); 
 
-function sortPoke() {
+function sortPoke(ka) {
   let namePoke = [];
 
   for (i in POKEMON["pokemon"]) {
     namePoke.push(POKEMON["pokemon"][i]["name"]);
-    if (selectOpt === 1) {
-      console.log("passou")
-      console.log("namePoke")
-      console.log(selectOpt)
+   
+    if (ka === 1) {
       namePoke.sort();
-    }
-    else if (selectOpt === 2) {
+    }else if (ka === 2) {
       console.log("passounoelseif")
       namePoke.sort();
       namePoke.reverse();
     }
-    // else{
-    //   showPokemons();
-    // }
-
-//     let pokemonDivFil = document.getElementById("list-poke");
-//     pokemonDivFil.innerHTML += `
-// <div class="pokemon-unit">
-//   <img src="${namePoke["img"]}" class="poke-img"/>
-//   <div class= "poke-namenum">
-//     <p class="poke-num"> Nº ${namePoke["num"]}</p>
-//     <h3 class="poke-name">${namePoke["name"]}</h3>
-//   </div> 
-// </div>
-// `
   }
+clearScreen();
+  for (let i of namePoke){
+    // console.log("NAMEP", i)
+    for (let j in POKEMON["pokemon"]) {
+      let nameData = POKEMON["pokemon"][j]["name"]
+      // console.log(nameData);
+      let pokeObj = POKEMON["pokemon"][j];
+      // console.log(pokeObj);
+      if (i === nameData){
+        console.log("NAMEP", i, nameData)
 
+         let pokemonDivFil = document.getElementById("list-poke");
+        pokemonDivFil.innerHTML += `
+    <div class="pokemon-unit">
+      <img src="${pokeObj["img"]}" class="poke-img"/>
+      <div class= "poke-namenum">
+        <p class="poke-num"> Nº ${pokeObj["num"]}</p>
+        <h3 class="poke-name">${pokeObj["name"]}</h3>
+      </div> 
+    </div>
+`
+      }
+    }
+  }
 }
