90b1555cb2ec3e731684fc21d978a711d2f66c8a
cakeno <camilla.noba@gmail.com>
2019-03-01 17:56:59 -0300

    starting function showModal

--- src/main.js
+++ src/main.js
@@ -1,62 +1,65 @@
-window.onload = function () {
-  showPokemons();
 
-};
+window.onload = function () {
+  showPokemons(getPokemons())
+  showModal('.pokemon-unit')
+}
 
 function getPokemons() {
   return POKEMON["pokemon"];
 }
 
-console.log("Lista de Pokemons: ", getPokemons())
-
-function showPokemons() {
+function showPokemons(getPoke) {
   let pokemonDiv = document.getElementById("list-poke");
   pokemonDiv.innerHTML = `
-    ${getPokemons().map((pokelist) => `
-      <div class="pokemon-unit">
-        <img src="${pokelist["img"]}" class="poke-img"/>
-        <div class= "poke-namenum">
-          <p class="poke-num"> Nº ${pokelist["num"]}</p>
-          <h3 class="poke-name">${pokelist["name"]}</h3>
+    ${getPoke.map((pokelist) => `
+      <div data-num=${pokelist["num"]} class="pokemon-unit">
+        <img data-num=${pokelist["num"]} src="${pokelist["img"]}" class="poke-img"/>
+        <div data-num=${pokelist["num"]} class= "poke-namenum">
+          <p data-num=${pokelist["num"]} class="poke-num"> Nº ${pokelist["num"]}</p>
+          <h3 data-num=${pokelist["num"]} class="poke-name">${pokelist["name"]}</h3>
         </div> 
       </div>
 `).join("")}
 `
 }
 
-let btnType = document.getElementsByClassName("btn-types")
-
-for (button of btnType){
-  let btnId = button.id;
- 
-  button.addEventListener('click', ()=>{
-    document.getElementById("list-poke").innerHTML="";  
-    filterPoke(btnId);
-  } );
-}
-
-function filterPoke(btnId){
-console.log(btnId)
-POKEMON["pokemon"].filter((elem)=> {elem.type.filter((ele) => {
-
-  
-  if (btnId === ele){
-
-
-let pokemonDivFil = document.getElementById("list-poketwo");
-pokemonDivFil.innerHTML = 
-   `
-    <div class="pokemon-unit">
-      <img src="${elem.img}" class="poke-img"/>
-      <div class= "poke-namenum">
-        <p class="poke-num"> Nº ${elem["num"]}</p>
-        <h3 class="poke-name">${elem["name"]}</h3>
+function filterPoke(btnId) {
+  POKEMON["pokemon"].filter((elem) => {
+    elem.type.filter((ele) => {
+      if (btnId === ele) {
+        let pokemonDivFil = document.getElementById("list-poketwo");
+        pokemonDivFil.innerHTML +=
+          `
+    <div} class="pokemon-unit">
+      <img data-num=${elem["num"]} src="${elem.img}" class="poke-img"/>
+      <div data-num=${elem["num"]} class= "poke-namenum">
+        <p data-num=${elem["num"]} class="poke-num" > Nº ${elem["num"]}</p>
+        <h3 data-num=${elem["num"]} class="poke-name">${elem["name"]}</h3>
       </div> 
     </div>
 `
+      }
+    })
+  })
 }
-  
-})} )
 
+let btnType = document.getElementsByClassName('btn-types')
 
+for (button of btnType) {
+  let btnId = button.id;
+
+  button.addEventListener('click', () => {
+    document.getElementById("list-poke").innerHTML = "";
+    filterPoke(btnId);
+  });
+}
+function showModal(classPokes) {
+  let pokesClass = document.querySelectorAll(classPokes)
+  for (let oneClass of pokesClass) {
+    oneClass.addEventListener('click', function (event) {
+      let dataNum = event.target.getAttribute("data-num")
+      
+    }
+    )
+  }
 }
