68bbaede2eaf7f6e03c9979fe29220a5bc140bb7
cakeno <camilla.noba@gmail.com>
2019-03-01 19:15:37 -0300

    get icons

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 
 window.onload = function () {
   showPokemons(getPokemons())
-  showModal('.pokemon-unit')
+  showModal('.poke-img')
+  showModal('.poke-namenum')
 }
 
 function getPokemons() {
@@ -10,13 +11,15 @@ function getPokemons() {
 
 function showPokemons(getPoke) {
   let pokemonDiv = document.getElementById("list-poke");
-  pokemonDiv.innerHTML = `
-    ${getPoke.map((pokelist) => `
-      <div data-num=${pokelist["num"]} class="pokemon-unit">
-        <img data-num=${pokelist["num"]} src="${pokelist["img"]}" class="poke-img"/>
-        <div data-num=${pokelist["num"]} class= "poke-namenum">
-          <p data-num=${pokelist["num"]} class="poke-num"> Nº ${pokelist["num"]}</p>
-          <h3 data-num=${pokelist["num"]} class="poke-name">${pokelist["name"]}</h3>
+  pokemonDiv.innerHTML =
+  `
+    ${getPoke.map((pokelist) =>
+      `
+      <div data-num=${pokelist.num} class="pokemon-unit">
+        <img data-num=${pokelist.num} class="poke-img" src="${pokelist.num}"/>
+        <div data-num=${pokelist.num} class= "poke-namenum">
+          <p data-num=${pokelist.num} class="poke-num"> Nº ${pokelist.num}</p>
+          <h3 data-num=${pokelist.num} class="poke-name">${pokelist.name}</h3>
         </div> 
       </div>
 `).join("")}
@@ -27,14 +30,14 @@ function filterPoke(btnId) {
   POKEMON["pokemon"].filter((elem) => {
     elem.type.filter((ele) => {
       if (btnId === ele) {
-        let pokemonDivFil = document.getElementById("list-poketwo");
+        let pokemonDivFil = document.getElementById("list-poke")
         pokemonDivFil.innerHTML +=
-          `
+        `
     <div} class="pokemon-unit">
-      <img data-num=${elem["num"]} src="${elem.img}" class="poke-img"/>
-      <div data-num=${elem["num"]} class= "poke-namenum">
-        <p data-num=${elem["num"]} class="poke-num" > Nº ${elem["num"]}</p>
-        <h3 data-num=${elem["num"]} class="poke-name">${elem["name"]}</h3>
+      <img data-num=${elem.num} src="${elem.img}" class="poke-img"/>
+      <div data-num=${elem.num} class= "poke-namenum">
+        <p data-num=${elem.num} class="poke-num" > Nº ${elem.num}</p>
+        <h3 data-num=${elem.num} class="poke-name">${elem.name}</h3>
       </div> 
     </div>
 `
@@ -53,13 +56,57 @@ for (button of btnType) {
     filterPoke(btnId);
   });
 }
+
 function showModal(classPokes) {
   let pokesClass = document.querySelectorAll(classPokes)
   for (let oneClass of pokesClass) {
     oneClass.addEventListener('click', function (event) {
+
       let dataNum = event.target.getAttribute("data-num")
-      
+      POKEMON["pokemon"].filter((elem) => {
+          if (dataNum === elem.num) {
+            document.querySelector('#sec-modal').style.display = "block";
+            document.querySelector('#sec-modal').innerHTML =
+            `
+            <section id="modal-content" class="modal-content">
+            <h1 class="title-name">${elem.name}</h1>
+            <div class="pic-description">
+                <figure id="photo-poke" class="poke-img">
+                <img src="${elem.img}"></figure>
+                <article class="poke-description">
+                  <ul>
+                    <li>N° ${elem.num}</li>
+                    <li>Altura: ${elem.height}</li>
+                    <li>Peso: ${elem.weight}</li>
+                    <li>${elem.candy}</li>
+                  </ul>
+                </article>
+            </div>
+            <div class="types-weak">
+              <div class="div-types">
+                <h2 class="subtitles">Tipos</h2>
+                ${elem.type.filter((elem) => {
+                  for(btn of document.querySelectorAll('.btn-types')){
+                    if (elem === btn.id){
+                      `
+                      <div class="div-types">
+                      ${console.log(btn.categ)}
+                      </div>
+                      `
+                    }
+                  }
+                })}
+              </div>
+              <div class="weak">
+                <h2 class="subtitles">Fraquezas</h2>
+              </div>
+            </div>
+            <h2 class="subtitles">Evoluções</h2>
+            <div class="sec-evol"></div>
+          </section>
+          `
+          }
+        })
+      })
     }
-    )
   }
-}
